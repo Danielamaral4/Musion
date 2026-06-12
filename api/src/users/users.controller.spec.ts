906fc9a39e27d144e4ca244fef6a9a37ab2ac0ef
Daniel Amaral <daniel_amaral99@hotmail.com>
2026-06-12 20:19:03 -0300

    Corrige teste do UsersController com AdminGuard

--- api/src/users/users.controller.spec.ts
+++ api/src/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { CloudinaryService } from '../cloudinary.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -12,6 +13,7 @@ describe('UsersController', () => {
       providers: [
         { provide: UsersService, useValue: {} },
         { provide: CloudinaryService, useValue: { uploadImage: jest.fn() } },
+        { provide: PrismaService, useValue: { user: { findUnique: jest.fn() } } },
       ],
     }).compile();
 
